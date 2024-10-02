72ff063c4af29be3d023605d952154c7e8720825
vanyakulinich <ivan@codox.io>
2024-10-02 15:31:24 +0300

    [update]: swift/kotlin for lex hidden

--- starters/swift-reactjs-lexical-codox/src/index.js
+++
@@ -1,8 +0,0 @@
-import './lexical/index.css';
-
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './lexical/App';
-
-createRoot(document.getElementById('root')).render(<App />);
